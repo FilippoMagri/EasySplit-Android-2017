01dc34f5c0b3f0688df0536ce1c71cd7a15dcd0b
FilippoMagri <fily.mag@gmail.com>
2017-04-01 17:46:48 +0200

    Add Filippo Activity

---
+++ app/src/main/java/it/polito/mad/easysplit/FilippoActivity.java
@@ -0,0 +1,14 @@
+package it.polito.mad.easysplit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class FilippoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filippo);
+    }
+}
