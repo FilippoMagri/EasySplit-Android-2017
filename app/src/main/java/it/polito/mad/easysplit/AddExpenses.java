d6bf754b9d1d6612e9685cec97b0a0ebeed4bac6
FilippoMagri <fily.mag@gmail.com>
2017-04-22 18:22:13 +0200

    //Avoiding Lose Data Fixed - Plus Other Stuff
    Other Stuff means:
    - A little Bit of refactoring
    - Setting the listener also on the Date Image (Positioned on the right of the date Edit Text)
    - Basically i used two json files , in the Cache of the mobile Phone. In this way the data is saved temporary and this strategy is data-stored Independent (In the case i'm reading from dummyGroup Created on the fly, or in the case i'm reading from a firebase Rest resource)
    - The JsonFile members is to manage the members into that specific group with the correct exchange between the activity AddExpenses and the Activity AddExpensesCheckBox
    - The JsonFile TemporaryInfo is to manage all important information that the user has already inserted and we dont want to lose during the exchange between the two activities

--- app/src/main/java/it/polito/mad/easysplit/AddExpenses.java
+++ app/src/main/java/it/polito/mad/easysplit/AddExpenses.java
@@ -1,17 +1,14 @@
 package it.polito.mad.easysplit;
 
-import android.app.Application;
 import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
-import android.util.Log;
-import android.view.Gravity;
+import android.util.JsonReader;
+import android.util.JsonWriter;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,15 +16,8 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Toast;
-
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-
-import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -36,19 +26,15 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
 import it.polito.mad.easysplit.models.ConstraintException;
-import it.polito.mad.easysplit.models.GroupModel;
 import it.polito.mad.easysplit.models.Money;
 import it.polito.mad.easysplit.models.PersonModel;
 import it.polito.mad.easysplit.models.dummy.DummyExpenseModel;
 import it.polito.mad.easysplit.models.dummy.DummyGroupModel;
 import it.polito.mad.easysplit.models.dummy.DummyPersonModel;
-
-import static android.os.Environment.getExternalStorageDirectory;
 
 public class AddExpenses extends AppCompatActivity {
     Toolbar toolbar;
@@ -56,65 +42,69 @@ public class AddExpenses extends AppCompatActivity {
     private EditText dateEditText;
     private DatePickerDialog datePickerDialog;
     private SimpleDateFormat dateFormatter;
-    private Spinner spinner1;
+    private Spinner spinner;
     private String payer;
-    DummyGroupModel gm;
-    List<PersonModel> lpm;
-    ArrayList<String> payerGroup;
-    ArrayList<PersonModel> payersEngaged = new ArrayList<PersonModel>();;
-    int size_group;
+    // TODO Fix phone_owner with the right reference to the unique id from Firebase
+    private String phone_owner = "Camille";
+    DummyGroupModel dummyGroupModel;
+    List<PersonModel> membersDummyGroup;
+    ArrayList<String> payerMembersSelected;
+    int sizeDummyGroupModel;
+    EditText editText1,editText2,editText3;
+    ImageView dateImgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses);
+        setDateEditText();
         try {
             populateWithInformationReceived ();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        setEditText_n1();
-        setActionButtons();
+        setActionOnButtons();
         addItemsOnSpinner();
-        setEditText_n5();
+        setEditText3();
     }
 
     public void populateWithInformationReceived () throws IOException {
         Intent received_intent = getIntent();
         if (received_intent.getExtras() == null ){
-            gm = new DummyGroupModel("Gruppo MAD");
+            dummyGroupModel = new DummyGroupModel("Gruppo MAD");
             try {
-                gm.addMember(new DummyPersonModel("Filippo",gm));
-                gm.addMember(new DummyPersonModel("Camille",gm));
-                gm.addMember(new DummyPersonModel("Sebastiano",gm));
-                gm.addMember(new DummyPersonModel("Flavio",gm));
+                dummyGroupModel.addMember(new DummyPersonModel("Filippo", dummyGroupModel));
+                dummyGroupModel.addMember(new DummyPersonModel("Camille", dummyGroupModel));
+                dummyGroupModel.addMember(new DummyPersonModel("Sebastiano", dummyGroupModel));
+                dummyGroupModel.addMember(new DummyPersonModel("Flavio", dummyGroupModel));
             } catch (ConstraintException e) {
                 e.printStackTrace();
             }
-            gm.writeIntoJsonFile(getCacheDir(),"group_members.json");
-            lpm = gm.getMembers();
-            size_group = gm.getMembers().size();
-            setTitle(gm.getName());
+            dummyGroupModel.writeIntoJsonFile(getExternalCacheDir(),"group_members.json");
+            membersDummyGroup = dummyGroupModel.getMembers();
+            sizeDummyGroupModel = dummyGroupModel.getMembers().size();
+            setTitle(dummyGroupModel.getName());
+            payer = phone_owner;
         } else {
-            if (received_intent.getExtras().get("Uniqid").equals("From_Activity_AddExpenses_checkBox")) {
-                //Populate again Screen By Reading file user.json
-                gm = new DummyGroupModel("Gruppo MAD");
-                gm.readFromJsonFile(getCacheDir(),"group_members.json");
-                lpm = gm.getMembers();
-                size_group = gm.getMembers().size();
-                setTitle(gm.getName());
+            if (received_intent.getExtras().get("Uniqid").equals("Activity_AddExpenses_checkBox")) {
+                //Populate again Screen By Reading file group_members.json
+                dummyGroupModel = new DummyGroupModel("Gruppo MAD");
+                dummyGroupModel.readFromJsonFile(getExternalCacheDir(),"group_members.json");
+                membersDummyGroup = dummyGroupModel.getMembers();
+                sizeDummyGroupModel = dummyGroupModel.getMembers().size();
+                setTitle(dummyGroupModel.getName());
                 //Retrieve Element Engaged into the payment
-                payerGroup = (ArrayList<String>) received_intent.getExtras().getSerializable("payerGroup");
-                if (payerGroup.size()<lpm.size()) {
-                    EditText editText5 = (EditText) findViewById(R.id.dateEditText5);
-                    editText5.setText("Custom Members");
+                payerMembersSelected = (ArrayList<String>) received_intent.getExtras().getSerializable("payerMembersSelected");
+                if (payerMembersSelected.size()< membersDummyGroup.size()) {
+                    editText3 = (EditText) findViewById(R.id.EditText3);
+                    editText3.setText("Custom Members");
                 }
-
+                readFromJsonFileTemporaryInformation(getExternalCacheDir() , "temporary_info.json");
             }
         }
     }
 
-    private void setEditText_n1 () {
+    private void setDateEditText() {
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ITALY);
         dateEditText = (EditText) findViewById(R.id.dateEditText);
         dateEditText.setInputType(InputType.TYPE_NULL);
@@ -142,7 +132,7 @@ public class AddExpenses extends AppCompatActivity {
         dateEditText.setText(dateFormatter.format(newDate.getTime()));
     }
 
-    public void setActionButtons() {
+    public void setActionOnButtons() {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         checkImgView = (ImageView) findViewById(R.id.img_confirm_add_expenses);
@@ -157,51 +147,56 @@ public class AddExpenses extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //Setting the Listener on the Image Of The Calendar
+        dateImgView = (ImageView) findViewById(R.id.dateImgView);
+        dateImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show();
+            }
+        });
         //Setting the Listener On the Check Image View On Top Right
         checkImgView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                EditText dateEditText1 = (EditText) findViewById(R.id.dateEditText);
-                String date = dateEditText1.getText().toString();
-
+                String date = dateEditText.getText().toString();
                 String dateStr = date;
-                SimpleDateFormat curFormater = new SimpleDateFormat("dd-MM-yyyy");
+                SimpleDateFormat curFormatter = new SimpleDateFormat("dd-MM-yyyy");
                 Date dateObj=null;
                 try {
-                    dateObj = curFormater.parse(dateStr);
+                    dateObj = curFormatter.parse(dateStr);
                 } catch(ParseException e) { e.printStackTrace();}
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTime(dateObj);
-                EditText dateEditText2 = (EditText) findViewById(R.id.dateEditText3);
-                String title = dateEditText2.getText().toString();
-                EditText dateEditText3 = (EditText) findViewById(R.id.dateEditText4);
-                String amount = dateEditText3.getText().toString();
+                editText1 = (EditText) findViewById(R.id.EditText1);
+                String title = editText1.getText().toString();
+                editText2 = (EditText) findViewById(R.id.EditText2);
+                String amount = editText2.getText().toString();
                 Money money = new Money(Money.getDefaultCurrency(),(long)(Float.parseFloat(amount)*100));
 
-                String payer_selected = payer;
-                DummyPersonModel dummyPersonModel_ofpayer  = new DummyPersonModel(payer,gm);
-                List<PersonModel> participants;
+                DummyPersonModel payer_AsDummyPersonModel  = new DummyPersonModel(payer, dummyGroupModel);
+                List<PersonModel> payerMemberSelected_asListPersonModel;
 
-                if (payerGroup != null) {
-                    participants = new ArrayList<>();
-                    for (int i = 0; i < payerGroup.size(); i++) {
-                        participants.add(gm.getMember(payerGroup.get(i)));
+                if (payerMembersSelected != null) {
+                    payerMemberSelected_asListPersonModel = new ArrayList<>();
+                    for (int i = 0; i < payerMembersSelected.size(); i++) {
+                        payerMemberSelected_asListPersonModel.add(dummyGroupModel.getMember(payerMembersSelected.get(i)));
                     }
                 }
                 else {
-                    participants = gm.getMembers();
+                    payerMemberSelected_asListPersonModel = dummyGroupModel.getMembers();
                 }
 
-                DummyExpenseModel expenseModel = new DummyExpenseModel(title,calendar,money,dummyPersonModel_ofpayer,gm, participants);
+                DummyExpenseModel expenseModel = new DummyExpenseModel(title,calendar,money,payer_AsDummyPersonModel, dummyGroupModel, payerMemberSelected_asListPersonModel);
                 try {
-                    gm.addExpense(expenseModel);
+                    dummyGroupModel.addExpense(expenseModel);
                 } catch (ConstraintException e) {
                     e.printStackTrace();
                 }
                 MyApplication app = (MyApplication) getApplicationContext();
                 if (app.getGroupModel()==null) {
-                    app.setGroupModel(gm);
+                    app.setGroupModel(dummyGroupModel);
                 } else {
                     app.getGroupModel().getExpenses().add(expenseModel);
                 }
@@ -213,17 +208,23 @@ public class AddExpenses extends AppCompatActivity {
     }
 
     public void addItemsOnSpinner() {
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner = (Spinner) findViewById(R.id.spinner);
         List<String> list = new ArrayList<String>();
-        int dim = lpm.size();
+        int dim = membersDummyGroup.size();
         for (int i=0;i<dim;i++) {
-            list.add(lpm.get(i).getIdentifier());
+            if (membersDummyGroup.get(i).getIdentifier().equals(payer)) {
+                list.add(membersDummyGroup.get(i).getIdentifier());
+            }
+        }
+        for (int i=0;i<dim;i++) {
+            if (!membersDummyGroup.get(i).getIdentifier().equals(payer)) {
+                list.add(membersDummyGroup.get(i).getIdentifier());
+            }
         }
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(dataAdapter);
-
-        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 payer = adapterView.getItemAtPosition(i).toString();
@@ -235,21 +236,93 @@ public class AddExpenses extends AppCompatActivity {
         });
     }
 
-    public void setEditText_n5 () {
-        EditText editText5 = (EditText)findViewById(R.id.dateEditText5);
-        if ((payerGroup==null)||(payerGroup.size()==lpm.size())) editText5.setText("All Members");
-        editText5.setInputType(InputType.TYPE_NULL);
-        editText5.setOnClickListener(new View.OnClickListener() {
+    public void setEditText3() {
+        editText3 = (EditText)findViewById(R.id.EditText3);
+        if ((payerMembersSelected ==null)||(payerMembersSelected.size()== membersDummyGroup.size())) editText3.setText("All Members");
+        editText3.setInputType(InputType.TYPE_NULL);
+        editText3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                try {
+                    writeIntoJsonFileTemporaryInformation(getExternalCacheDir(),"temporary_info.json");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 Intent intent = new Intent(getApplicationContext(),AddExpenses_checkBox.class);
                 Bundle b = new Bundle();
-                intent.putExtra("group_info",gm.toJSON());
+                intent.putExtra("members_info", dummyGroupModel.toJSON());
                 startActivity(intent);
             }
         });
     }
 
+    public void writeIntoJsonFileTemporaryInformation(File fileDir,String nameFile) throws IOException{
+        dateEditText = (EditText)findViewById(R.id.dateEditText);
+        editText1 = (EditText) findViewById(R.id.EditText1);
+        editText2 = (EditText) findViewById(R.id.EditText2);
+        JsonWriter writer;
+        File file = new File(fileDir , nameFile);
+        writer = new JsonWriter(new FileWriter(file.getPath()));
+        writer.beginObject();
+        writer.name("expense_date").value(dateEditText.getText().toString());
+        if(editText1 == null) {
+            writer.name("expense_title").value("");
+        } else {
+            writer.name("expense_title").value(editText1.getText().toString());
+        }
+        if (editText2 == null ) {
+            writer.name("expense_amount").value("");
+        } else {
+            writer.name("expense_amount").value(editText2.getText().toString());
+        }
+        writer.name("expense_payer").value(payer);
+        if(payerMembersSelected!=null) {
+            writer.name("payer_members_selected");
+            if (payerMembersSelected.size() > 0) {
+                writer.beginArray();
+                for (int i = 0; i < payerMembersSelected.size(); i++) {
+                    writer.value(payerMembersSelected.get(i));
+                }
+                writer.endArray();
+            } else {
+                writer.value("");
+            }
+        }
+        writer.endObject();
+        writer.close();
+
+    }
+
+    public void readFromJsonFileTemporaryInformation(File fileDir,String nameFile) throws IOException {
+        JsonReader reader = new JsonReader(new FileReader(fileDir.toString()+"/"+nameFile));
+        reader.beginObject();
+        while (reader.hasNext()) {
+            dateEditText = (EditText)findViewById(R.id.dateEditText);
+            editText1 = (EditText) findViewById(R.id.EditText1);
+            editText2 = (EditText) findViewById(R.id.EditText2);
+            String name = reader.nextName();
+            String value="";
+            if (name.equals("expense_date")) {
+                value = reader.nextString();
+                dateEditText.setText(value);
+            } else if (name.equals("expense_title")) {
+                value = reader.nextString();
+                editText1.setText(value);
+            } else if (name.equals("expense_amount")) {
+                value = reader.nextString();
+                editText2.setText(value);
+            } else if (name.equals("expense_payer")) {
+                value = reader.nextString();
+                payer = value;
+            }else {
+                reader.skipValue(); //avoid some unhandle events
+            }
+        }
+
+        reader.endObject();
+        reader.close();
+
+    }
 
 
 }
