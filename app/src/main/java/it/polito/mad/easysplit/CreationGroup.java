2af3e42abd7877694819233724da9b0388c6f9e1
FilippoMagri <fily.mag@gmail.com>
2017-05-20 12:10:04 +0200

    Temporary Feature On Creation Group Deactivated
    The possibility of adding multiple users directly from Creation Group had cause some glitch problems in Group Screen, probably because a bad managing of thread in android.
    For this reason , in order to give to Anna a stable version , where is always possible to add users to the group by invite inside the group it self, we decided to de-activate this functionality from CreationGroup Temporary.
    Temporary because maybe @Flavio will find a fix for this problem.

--- app/src/main/java/it/polito/mad/easysplit/CreationGroup.java
+++ app/src/main/java/it/polito/mad/easysplit/CreationGroup.java
@@ -43,7 +43,6 @@ public class CreationGroup extends AppCompatActivity {
         setContentView(R.layout.activity_creation_group);
 
         final EditText groupNameEdit = (EditText) findViewById(R.id.nameGroup);
-        final EditText participantsListEdit = (EditText) findViewById(R.id.newGroupParticipantsList);
         ImageView submit = (ImageView) findViewById(R.id.valid);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -57,148 +56,26 @@ public class CreationGroup extends AppCompatActivity {
             }
         });
 
-        setTitle("Group Creation");
-        TextWatcher tw = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            private int getColorFromString(String str) {
-                return isValidEmail(str) ? Color.GREEN : Color.RED;
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                Character[] spacingChars = {' ', '\t', '\n'};
-
-                String str = s.toString();
-                SpannableString text = new SpannableString(str);
-                int position = participantsListEdit.getSelectionStart();
-                int start;
-                for (start = 0; start < s.length(); ) {
-                    int end = start;
-                    while (end < str.length() && !Arrays.asList(spacingChars).contains(str.charAt(end))) {
-                        end++;
-                    }
-                    text.setSpan(new BackgroundColorSpan(getColorFromString(str.substring(start, end))), start, end, 0);
-                    start = end + 1;
-                    while (start < str.length() && Arrays.asList(spacingChars).contains(str.charAt(start))) {
-                        start++;
-                    }
-                }
-
-                /* set new text (with highlights) */
-                participantsListEdit.removeTextChangedListener(this);
-                participantsListEdit.setText(text);
-                participantsListEdit.addTextChangedListener(this);
-                participantsListEdit.setSelection(position); // update to previous position */
-            }
-        };
-        participantsListEdit.addTextChangedListener(tw);
-
+        setTitle(R.string.creation_group_title);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final DatabaseReference root = FirebaseDatabase.getInstance().getReference();
-                root.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(final DataSnapshot usersSnap) {
-                        String participantsStr = participantsListEdit.getText().toString();
-                        final List<String> participants = Arrays.asList(participantsStr.split("\\s+"));
-                        final String groupName = groupNameEdit.getText().toString();
-
-                        Tasks.call(new Callable<Void>() {
-                            @Override
-                            public Void call() throws Exception {
-                                createGroup(groupName, participants, usersSnap);
-                                return null;
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
+                final String groupName = groupNameEdit.getText().toString();
+                createGroup(groupName);
 
             }
         });
 
     }
 
-    private boolean isValidEmail(String str) {
-        // TODO Add checks on email validity here!
-        return str.matches("^[\\w\\.]+@[\\w\\.]+\\.[a-z]+$");
-    }
-
-    private void createGroup(String groupName, List<String> emails, DataSnapshot usersSnap) {
-        ArrayList<String> externalEmails = new ArrayList<>(emails);
+    private void createGroup(String groupName) {
         HashMap<String, String> groupMembers = new HashMap<>();
         HashMap<String, Object> childUpdates = new HashMap<>();
 
         String groupKey = mRoot.child("groups").push().getKey();
 
-        // create an internal mapping email -> uid
-        for (DataSnapshot user : usersSnap.getChildren()) {
-            if (!user.hasChild("email"))
-                continue;
-
-            String emailAddr = user.child("email").getValue(String.class);
-            if (!emails.contains(emailAddr))
-                continue;
-
-            // Okay, email required by the user! include it
-            // (UID used as key, not the email!)
-            String userName = user.child("name").getValue().toString();
-            groupMembers.put(user.getKey(), userName);
-            childUpdates.put("/users/" + user.getKey() + "/groups_ids/" + groupKey, groupName);
-            externalEmails.remove(emailAddr); // user found, no need to register it
-        }
-
-        // proceed with the registration of the new email accounts!
-        for (String email : externalEmails) {
-            if (isValidEmail(email))
-                continue;
-
-            FirebaseAuth auth = FirebaseAuth.getInstance();
-            Task<AuthResult> task = auth.createUserWithEmailAndPassword(email, InvitePerson.defaultTemporaryUserRegistrationPassword);
-            try {
-                Tasks.await(task);
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-
-            FirebaseUser user = task.getResult().getUser();
-            String userId = user.getUid();
-
-            HashMap<String, String> groupsMap = new HashMap<>();
-            groupsMap.put(groupKey, groupName); // add group to the user
-
-            HashMap<String, Object> newUser = new HashMap<>();
-            newUser.put("email", email);
-            newUser.put("name", email); // use email as the name (until specified by the user upon registration)
-            newUser.put("groups_ids", groupsMap);
-
-            childUpdates.put("/users/" + userId, newUser); // add new user to database
-            groupMembers.put(userId, email); // add user to the group
-            // TODO Because in this case Flavio has expected the functionality of invite a new user by just the email, we'll have to introduce also here the invitation-mail. (Later on)
-        }
-
         SharedPreferences sharedPref = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
-        String signin_email = sharedPref.getString("signin_email", null);
-        String signin_password = sharedPref.getString("signin_password", null);
         String signin_complete_name = sharedPref.getString("signin_complete_name", null);
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        try {
-            Tasks.await(auth.signInWithEmailAndPassword(signin_email, signin_password));
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
 
         // Finally, add current user
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -207,7 +84,7 @@ public class CreationGroup extends AppCompatActivity {
 
         HashMap<String, Object> map = new HashMap<>();
         map.put("name", groupName);
-        map.put("expenses_ids", new HashMap<String, Boolean>());
+        map.put("expenses", new HashMap<String, Boolean>());
         map.put("members_ids", groupMembers);
 
         childUpdates.put("/groups/" + groupKey, map);
