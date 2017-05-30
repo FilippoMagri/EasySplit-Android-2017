f716501462a11c3b5557c6b9fa16c9788287134d
FilippoMagri <fily.mag@gmail.com>
2017-05-30 17:22:56 +0200

    Added Interaction between GroupBalanceModel and Payment Layout

--- app/src/main/java/it/polito/mad/easysplit/Payment.java
+++ app/src/main/java/it/polito/mad/easysplit/Payment.java
@@ -1,18 +1,78 @@
 package it.polito.mad.easysplit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import it.polito.mad.easysplit.models.Money;
 
 public class Payment extends AppCompatActivity {
+
+    private CurrencySpinnerAdapter mCurrenciesAdapter;
+    private TextView mTvCreditor;
+    private TextView mTvDebtor;
+    private EditText mEditTextPayment;
+    //RootMember
+    String rootMemberName = "";
+    String rootMemberId = "";
+    String rootMemberMoney = "";
+    String rootMemberSymbol = "";
+    String rootMemberCurrency = "";
+    //Sub Member
+    String subMemberName = "";
+    String subMemberId = "";
+    String subMemberMoney = "";
+    String subMemberSymbol = "";
+    String subMemberCurrency = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        retrieveIntentInformations();
+
+        Log.d("Payment","RootMemberName: "+rootMemberName);
+        Log.d("Payment","SubMemberName: "+subMemberName);
+        Log.d("Payment","SubMemberMoney"+subMemberMoney);
+
+        mCurrenciesAdapter = new CurrencySpinnerAdapter(this);
+
+        mTvCreditor = (TextView) findViewById(R.id.textview_creditor);
+        mTvDebtor = (TextView) findViewById(R.id.textview_debtor);
+        mEditTextPayment = (EditText) findViewById(R.id.amountPayment);
+
+        Money moneyCheck = new Money(Currency.getInstance(rootMemberCurrency),new BigDecimal(rootMemberMoney));
+        Money amountPayment = new Money(Currency.getInstance(subMemberCurrency),new BigDecimal(subMemberMoney));
+        mEditTextPayment.setText(amountPayment.abs().toString().replace(subMemberSymbol,"").replace(" ",""));
+
+        if (moneyCheck.cmpZero()>0) {
+            //RootMember is a creditor
+            mTvCreditor.setText(rootMemberName);
+            mTvDebtor.setText(subMemberName);
+        } else if (moneyCheck.cmpZero()<0){
+            //SubMember is a creditor
+            mTvCreditor.setText(subMemberName);
+            mTvDebtor.setText(rootMemberName);
+        }
+
+        Spinner currencySpinner = (Spinner) findViewById(R.id.paymentSpinner);
+        currencySpinner.setAdapter(mCurrenciesAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -20,10 +80,37 @@ public class Payment extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar snack = Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG);
+                View viewSnack = snack.getView();
+                CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams)viewSnack.getLayoutParams();
+                params.gravity = Gravity.TOP;
+                viewSnack.setLayoutParams(params);
+                snack.setAction("Action", null);
+                snack.show();
             }
         });
+    }
+
+    private void retrieveIntentInformations() {
+        Intent intent = getIntent();
+        //RootMember
+        this.rootMemberName = intent.getStringExtra("RootMemberName");
+        this.rootMemberId = intent.getStringExtra("RootMemberId");
+        this.rootMemberMoney = intent.getStringExtra("RootMemberMoney");
+        this.rootMemberSymbol = intent.getStringExtra("RootMemberSymbol");
+        this.rootMemberCurrency = intent.getStringExtra("RootMemberCurrency");
+        //Adjust String formats
+        rootMemberMoney = rootMemberMoney.replace(rootMemberSymbol,"");
+        rootMemberMoney = rootMemberMoney.replace(" ","");
+        //Sub Member
+        this.subMemberName = intent.getStringExtra("SubMemberName");
+        this.subMemberId = intent.getStringExtra("SubMemberId");
+        this.subMemberMoney = intent.getStringExtra("SubMemberMoney");
+        this.subMemberSymbol = intent.getStringExtra("SubMemberSymbol");
+        this.subMemberCurrency = intent.getStringExtra("SubMemberCurrency");
+        //Adjust String formats
+        subMemberMoney = subMemberMoney.replace(subMemberSymbol,"");
+        subMemberMoney = subMemberMoney.replace(" ","");
     }
 
 }
--- app/src/main/java/it/polito/mad/easysplit/layout/GroupBalanceAdapter.java
+++ app/src/main/java/it/polito/mad/easysplit/layout/GroupBalanceAdapter.java
@@ -1,18 +1,22 @@
 package it.polito.mad.easysplit.layout;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Map;
 
+import it.polito.mad.easysplit.Payment;
 import it.polito.mad.easysplit.R;
 import it.polito.mad.easysplit.R.id;
 import it.polito.mad.easysplit.R.layout;
@@ -41,10 +45,10 @@ class GroupBalanceAdapter extends ArrayAdapter<MemberRepresentation> implements 
             convertView = inflater.inflate(layout.list_item_member, parent, false);
         }
 
-        MemberRepresentation memberRepr = getItem(position);
+        final MemberRepresentation memberRepr = getItem(position);
         TextView name = (TextView) convertView.findViewById(id.name);
         TextView residueText = (TextView) convertView.findViewById(id.residue);
-        TextView typeOfMember = (TextView) convertView.findViewById(id.typeOfMember);
+        final TextView typeOfMember = (TextView) convertView.findViewById(id.typeOfMember);
         TextView keyItemMember = (TextView) convertView.findViewById(id.key_item_member);
 
         // access to nested linear layout, in order to attach the list of creditors or debtors
@@ -76,12 +80,13 @@ class GroupBalanceAdapter extends ArrayAdapter<MemberRepresentation> implements 
         }
 
         for (Map.Entry<MemberRepresentation, Money> entry : memberRepr.getConvertedAssignments().entrySet()) {
-            MemberRepresentation debtor = entry.getKey();
-            Money debt = entry.getValue();
+            final MemberRepresentation debtor = entry.getKey();
+            final Money debt = entry.getValue();
 
             View child = LayoutInflater.from(getContext()).inflate(R.layout.catch_up_group_item, parent, false);
             TextView messageText = (TextView) child.findViewById(R.id.text_view_catch_up_item);
             TextView debtText = (TextView) child.findViewById(R.id.residue_catch_up_item);
+            ImageView imageView = (ImageView) child.findViewById(R.id.ic_next_catch_up_item);
 
             int messageRes = cmp > 0 ? R.string.balance_debtor_message : R.string.balance_creditor_message;
             String messageFmt = getContext().getResources().getString(messageRes);
@@ -89,9 +94,45 @@ class GroupBalanceAdapter extends ArrayAdapter<MemberRepresentation> implements 
             messageText.setText(message);
 
             debtText.setText(debt.abs().toString());
+
+            OnClickCatchUpItemListener onClickCatchUpItemListener = new OnClickCatchUpItemListener(memberRepr,debtor,debt,getContext());
+            messageText.setOnClickListener(onClickCatchUpItemListener);
+            debtText.setOnClickListener(onClickCatchUpItemListener);
+            imageView.setOnClickListener(onClickCatchUpItemListener);
             layout.addView(child);
         }
-
         return convertView;
+    }
+}
+
+class OnClickCatchUpItemListener implements View.OnClickListener {
+    MemberRepresentation memberRepr,debtor;
+    Money debt;
+    Context context;
+
+    public OnClickCatchUpItemListener(MemberRepresentation memberRepr, MemberRepresentation debtor,Money debt, Context context) {
+        this.memberRepr = memberRepr;
+        this.debtor = debtor;
+        this.debt = debt;
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(context, Payment.class);
+        //This Will be "Credtor","Name" or "Debtor","Name"
+        //Root Member
+        intent.putExtra("RootMemberName",memberRepr.getName());
+        intent.putExtra("RootMemberId",memberRepr.getId());
+        intent.putExtra("RootMemberMoney",memberRepr.getResidue().toString());
+        intent.putExtra("RootMemberSymbol",memberRepr.getResidue().getCurrency().getSymbol());
+        intent.putExtra("RootMemberCurrency",memberRepr.getResidue().getCurrency().toString());
+        //Sub Member
+        intent.putExtra("SubMemberName",debtor.getName());
+        intent.putExtra("SubMemberId",debtor.getId());
+        intent.putExtra("SubMemberMoney",debt.toString());
+        intent.putExtra("SubMemberSymbol",debt.getCurrency().getSymbol());
+        intent.putExtra("SubMemberCurrency",debt.getCurrency().toString());
+        context.startActivity(intent);
     }
 }
