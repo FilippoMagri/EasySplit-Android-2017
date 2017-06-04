3e24c819e1920db4841ac1dd2bf4fe3009459086
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2017-06-04 10:54:10 +0200

    add profile settings, profile picture

---
+++ app/src/main/java/it/polito/mad/easysplit/ProfilePictureManager.java
@@ -0,0 +1,198 @@
+package it.polito.mad.easysplit;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class ProfilePictureManager {
+    public static final int THUMBNAIL_SIZE = 200;
+    public static final long RELOAD_PERIOD_MILLIS = TimeUnit.SECONDS.convert(10, TimeUnit.MINUTES);
+
+    //
+    // Maintain a single instance (at the most) for each user, so different activities can share
+    // the cache.
+    //
+    private static final HashMap<String, WeakReference<ProfilePictureManager>> sInstances = new HashMap<>();
+    private Bitmap mPicture, mThumbnail;
+    private File mLocalFile;
+    private final Handler mHandler = new Handler();
+
+    public static ProfilePictureManager forUser(Context ctx, Uri userUri) {
+        return forUser(ctx, Utils.getIdFor(Utils.UriType.USER, userUri));
+    }
+    public static ProfilePictureManager forUser(Context ctx, String userId) {
+        WeakReference<ProfilePictureManager> instance = sInstances.get(userId);
+
+        if (instance == null || instance.get() == null) {
+            ProfilePictureManager newInstance = new ProfilePictureManager(ctx, userId);
+            sInstances.put(userId, new WeakReference<>(newInstance));
+            return newInstance;
+        }
+
+        return instance.get();
+    }
+
+    public interface Listener {
+        void onPictureReceived(Bitmap pic);
+        void onThumbnailReceived(Bitmap pic);
+        void onFailure(Exception e);
+    }
+
+    private final String mProfilePicFilename;
+    private StorageReference mStorageRef;
+    private Context mContext;
+    private ArrayList<Listener> mListeners = new ArrayList<>();
+
+
+    private ProfilePictureManager(Context ctx, String userId) {
+        mContext = ctx;
+        mProfilePicFilename = "profilepic-" + userId;
+        mStorageRef = FirebaseStorage.getInstance().getReference().child(mProfilePicFilename);
+
+        String cacheDir = mContext.getCacheDir().getAbsolutePath();
+        mLocalFile = new File(cacheDir + "/" + mProfilePicFilename);
+
+        if (mLocalFile.exists()) {
+            // Start by loading the cache
+            Bitmap pic = BitmapFactory.decodeFile(mLocalFile.getAbsolutePath());
+            notifyProfilePicture(pic);
+        } else {
+            // No cache, load from Firebase
+            reload();
+        }
+
+        // Reload every RELOAD_PERIOD_MILLIS milliseconds
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                reload();
+                mHandler.postDelayed(this, RELOAD_PERIOD_MILLIS);
+            }
+        }, RELOAD_PERIOD_MILLIS);
+    }
+
+    void reload() {
+        mStorageRef.getFile(mLocalFile)
+                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Bitmap pic = BitmapFactory.decodeFile(mLocalFile.getAbsolutePath());
+                        notifyProfilePicture(pic);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        notifyFailure(e);
+                    }
+                });
+    }
+
+    public void addListener(Listener listener) {
+        mListeners.add(listener);
+        if (mPicture != null)
+            listener.onPictureReceived(mPicture);
+        if (mThumbnail != null)
+            listener.onThumbnailReceived(mThumbnail);
+    }
+
+    public void removeListener(Listener listener) {
+        mListeners.remove(listener);
+    }
+
+    private void notifyProfilePicture(Bitmap picture) {
+        // Create scaled version of the picture, maintaining the aspect ratio
+        double ratio = (double)picture.getWidth() / picture.getHeight();
+        int dstWidth, dstHeight;
+        if (picture.getWidth() > picture.getHeight()) {
+            dstWidth = THUMBNAIL_SIZE;
+            dstHeight = (int)(dstWidth / ratio);
+        } else {
+            dstHeight = THUMBNAIL_SIZE;
+            dstWidth = (int)(dstHeight * ratio);
+        }
+
+        Bitmap thumbnail = Bitmap.createScaledBitmap(picture, dstWidth, dstHeight, true);
+
+        mPicture = picture;
+        mThumbnail = thumbnail;
+
+        for (Listener listener : new ArrayList<>(mListeners)) {
+            listener.onPictureReceived(picture);
+            listener.onThumbnailReceived(thumbnail);
+        }
+    }
+
+    private Bitmap cropCircular(Bitmap originalPicture) {
+        int smallestSide = Math.min(originalPicture.getWidth(), originalPicture.getHeight());
+        int radius = smallestSide / 2;
+
+        Bitmap output = Bitmap.createBitmap(smallestSide, smallestSide, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        Paint paint = new Paint();
+        Rect rect = new Rect(0, 0, smallestSide, smallestSide);
+
+        paint.setAntiAlias(true);
+        paint.setFilterBitmap(true);
+        paint.setDither(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(Color.argb(255, 255, 255, 255));
+        canvas.drawCircle(radius, radius, radius, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(originalPicture, rect, rect, paint);
+
+        return output;
+    }
+
+    private void notifyFailure(Exception e) {
+        for (Listener listener : new ArrayList<>(mListeners))
+            listener.onFailure(e);
+    }
+
+    public void setPicture(Bitmap originalBitmap) {
+        // TODO Possibly more efficient method?
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        final Bitmap bitmap = cropCircular(originalBitmap);
+        bitmap.compress(Bitmap.CompressFormat.WEBP, 90, stream);
+
+        mStorageRef.putStream(new ByteArrayInputStream(stream.toByteArray()))
+            .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.getException() != null) {
+                        notifyFailure(task.getException());
+                        return;
+                    }
+
+                    notifyProfilePicture(bitmap);
+                }
+            });
+    }
+}
---
+++ app/src/main/java/it/polito/mad/easysplit/UserProfileActivity.java
@@ -0,0 +1,167 @@
+package it.polito.mad.easysplit;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserProfileActivity extends AppCompatActivity {
+    private DatabaseReference mUser;
+
+    private EditText mNameEdit;
+    private TextView mEmailText;
+    private ImageButton mImageButton;
+    private ProgressBar mProfilePicProgressBar;
+
+    private ProfilePictureManager mPicManager;
+
+    private void setInProgress(boolean inProgress) {
+        setProgressBarIndeterminateVisibility(inProgress);
+        mImageButton.setVisibility(inProgress ? View.INVISIBLE : View.VISIBLE);
+        mProfilePicProgressBar.setVisibility(inProgress ? View.VISIBLE : View.INVISIBLE);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_user_profile);
+
+        mNameEdit = (EditText) findViewById(R.id.nameEdit);
+        mEmailText = (TextView) findViewById(R.id.emailText);
+        mImageButton = (ImageButton) findViewById(R.id.imageButton);
+        mProfilePicProgressBar = (ProgressBar) findViewById(R.id.profilePicProgressBar);
+
+        setProgressBarIndeterminateVisibility(true);
+        mImageButton.setVisibility(View.INVISIBLE);
+        mProfilePicProgressBar.setVisibility(View.VISIBLE);
+
+        Uri userUri = getIntent().getData();
+        String userId = Utils.getIdFor(Utils.UriType.USER, userUri);
+
+        mUser = Utils.findByUri(userUri);
+        mUser.child("name").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot nameSnap) {
+                mNameEdit.setText(nameSnap.getValue(String.class));
+                setProgressBarIndeterminateVisibility(false);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                showDatabaseError(databaseError);
+            }
+        });
+
+        mUser.child("email").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot emailSnap) {
+                mEmailText.setText(emailSnap.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                showDatabaseError(databaseError);
+            }
+        });
+
+
+        mPicManager = ProfilePictureManager.forUser(this, userId);
+        mPicManager.addListener(new ProfilePictureManager.Listener() {
+            @Override
+            public void onThumbnailReceived(Bitmap pic) {
+
+            }
+
+            @Override
+            public void onPictureReceived(Bitmap pic) {
+                mProfilePicProgressBar.setVisibility(View.INVISIBLE);
+                mImageButton.setVisibility(View.VISIBLE);
+                mImageButton.setImageBitmap(pic);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                mProfilePicProgressBar.setVisibility(View.INVISIBLE);
+                mImageButton.setVisibility(View.VISIBLE);
+                mImageButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_default_profile_pic));
+            }
+        });
+
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeProfilePic();
+            }
+        });
+    }
+
+    void showDatabaseError(DatabaseError databaseError) {
+        setInProgress(false);
+        new AlertDialog.Builder(UserProfileActivity.this)
+                .setCancelable(false)
+                .setTitle("We're sorry!")
+                .setMessage("There was a problem: " + databaseError.getMessage())
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        UserProfileActivity.this.finish();
+                    }
+                })
+                .show();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        updateName();
+    }
+
+    private void updateName() {
+        String newName = mNameEdit.getText().toString();
+        mUser.child("name").setValue(newName);
+    }
+
+    private static final int RESULT_PROFILE_PIC = 1;
+
+    public void changeProfilePic() {
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        i.setType("image/*");
+        i.putExtra("crop", "true");
+        i.putExtra("return-data", true);
+        i.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        startActivityForResult(i, RESULT_PROFILE_PIC);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null || resultCode != RESULT_OK)
+            return;
+
+        if (requestCode == RESULT_PROFILE_PIC) {
+            Bundle extras = data.getExtras();
+            Bitmap selectedBitmap = extras.getParcelable("data");
+            mPicManager.setPicture(selectedBitmap);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
